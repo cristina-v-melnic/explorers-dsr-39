0968b389c14fde6e8ab2a23afc21b5e303d701d2
Julien Henry <julien.henry@inrae.fr>
2024-07-19 10:00:36 +0200

    started building dummy pipeline

--- src/explorers_dsr_39/pipelines/data_processing/nodes.py
+++ src/explorers_dsr_39/pipelines/data_processing/nodes.py
@@ -1,5 +1,49 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+def get_train_data(features, labels):
+    """ 
+    Input:
+        str to csv with train features and labels
+
+    Output:
+        data_clean_dt, X, y
+    
+    """
+    data = features.merge(labels)
+
+    data_clean = data.dropna()
+    print("Droped {:.2f}% of rows.".format((1 - data_clean.shape[0]/data.shape[0])*100))
+
+    data_clean.loc[:,'log_total_cases'] = np.log1p(data_clean['total_cases'])
+    data_clean_dt = get_dt_col(data_clean)
+
+    y = data_clean_dt['log_total_cases']
+    X = data_clean_dt.drop(columns=['total_cases','log_total_cases'])
+
+    
+    return data_clean_dt, X, y
 
 
-def impute_data(Xtrain):
-    return Xtrain.dropna(axis=0)
+def get_dt_col(X , column='week_start_date', format='%Y-%m-%d'):
+    """
+    Input:
+        X
+        col = weekofx
+
+    Returns:
+        X with X[col].dtype = datetime
+
+    """
+    X_new = X.copy()
+    X_new[column] = pd.to_datetime(X_new[column], format=format)
+
+    return X_new
+
+def get_model(X, y, feats):
+    regr = RandomForestRegressor(random_state=0)
+    regr.fit(X[feats], y)
+    return regr
+
+def get_pred(regr, test_data, feats):
+    predictions = regr.predict(test_data[feats])
--- src/explorers_dsr_39/pipelines/data_processing/pipeline.py
+++ src/explorers_dsr_39/pipelines/data_processing/pipeline.py
@@ -1,16 +1,28 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import impute_data
+from .nodes import get_train_data, get_model, get_pred
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=impute_data,
-                inputs="train_data",
-                outputs="train_data_dropna",
-                name="example_pipeline",
+                func=get_train_data,
+                inputs=["train_data","labels"],
+                outputs=["data_clean_dt", "X", "y"],
+                name="pipeline_part1",
+            ),
+            node(
+                func=get_model,
+                inputs=["X","y","params:feats"],
+                outputs="model",
+                name="model",
+            ),
+            node(
+                func=get_pred,
+                inputs=["model","test_data","params:feats"],
+                outputs="results",
+                name="pred",
             )
         ]
     )
